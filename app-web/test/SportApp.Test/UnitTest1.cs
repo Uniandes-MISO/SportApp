75c26ee68dc0d4758fd5c98d87cb562c33b95c18
wilson-bg <w.beltran@uniandes.edu.co>
2022-10-16 16:22:40 -0500

    Add UnitTest

---
+++ app-web/test/SportApp.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SportApp.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
